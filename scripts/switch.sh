4f8998779ee53c851aa8fe60aebaa421339330ca
eunho.hong <wisdombrave@gmail.com>
2021-05-06 15:47:15 +0900

    nginx conf 설정 필요

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,6 +9,7 @@ function switch_proxy() {
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
+    #nginx conf에서 service-url.inc를 include하도록 파일 수정 필요.
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
     echo "> 엔진엑스 Reload"
